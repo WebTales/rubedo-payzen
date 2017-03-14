b7c78378c0f44de390e82adb2f6e508028f67d2b
Alexandru Dobre <alexandru.dobre@webtales.fr>
2017-03-14 15:21:40 +0100

    WIP payment form

--- src/RubedoPayzen/Payment/PaypalPayment.php
+++ src/RubedoPayzen/Payment/PaypalPayment.php
@@ -38,6 +38,57 @@ class PayzenPayment extends AbstractPayment
     public function getOrderPaymentData($order,$currentUserUrl)
     {
         $toolbox=new Toolbox($this->nativePMConfig);
-        die("test");
+        $centPrice=$order['finalPrice']*100;
+        $args = array(
+            'vads_amount' => array(
+                'value' => (string) intval($centPrice),
+                'label' => 'Price',
+                'type' => 'text',
+                'class'  => 'vads-field',
+                'wrapper_class' => 'vads-wrapper',
+                'readonly' => true,
+                'help' =>  'Price'
+            ),
+            "vads_currency" => "978",
+            "vads_order_id" => (string) $order["id"]
+        );
+        $formData = $toolbox->getFormData($args);
+        $form = '<form id="payzenAutoSubmitForm" action="'.$formData['form']['action'].'" method="'.$formData['form']['method'].'" accept-charset="'.$formData['form']['accept-charset'].'" class="form-horizontal">';
+        foreach ($formData['fields'] as $name => $value) {
+            $display_value = (isset($value['value']) && is_array($value)) ? $value['value'] : $value;
+            $label = (isset($value['label']) && is_array($value)) ? $value['label'] : $name;
+            $class = (isset($value['class']) && is_array($value)) ? $value['class'] : '';
+            $help = (isset($value['help']) && $value['help'] !== '' && is_array($value)) ? ' '.$value['help'] : '';
+            $wrapper_class = (isset($value['wrapper_class']) && is_array($value)) ? $value['wrapper_class'] : 'hidden';
+            $type = (isset($value['type']) && is_array($value) ) ? $value['type'] : 'text';
+            $help_link = '<small id="helpBlock" class="help-block">'.$help.'</small>';
+            $addon = '';
+            $addon_end = '';
+            $hidden_field = '';
+            if($name == 'vads_amount'){
+                $hidden_field = '<input type="hidden" value="'.$display_value.'" name="vads_amount"/>';
+                $cents =  substr($display_value,-2);
+                $amount = substr($display_value,0,-2);
+                $display_value = $amount.','.$cents;
+                $addon = '<div class="input-group">';
+                $addon_end = '<span class="glyphicon glyphicon-euro form-control-feedback" aria-hidden="true"></span></div>';
+            }
+            //$form .= $hidden_field;
+            $form .= '<div class="form-group '.$wrapper_class.'">';
+            $form .= '<label for="'. $name. '" class="col-sm-2 control-label">'.$label.'</label>';
+            $form .= '<div class="col-sm-10">';
+            $form .= $addon;
+            $form .= $hidden_field;
+            $form .= '<input type="'.$type.'" readonly="readonly"  class="form-control '.$class.'"  name="'.$name.'" value="'.$display_value.'" />';
+            $form .= $help_link;
+            $form .= $addon_end;
+            $form .= '</div></div>';
+        }
+        $form .= '<button type="submit" class="btn btn-default">Payer</button>';
+        $form .= '</form><script>document.getElementById("payzenAutoSubmitForm").submit();</script>';
+        return [
+            'whatToDo'=>'displayRichText',
+            'richText'=>$form
+        ];
     }
 }
